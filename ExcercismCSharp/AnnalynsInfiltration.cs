36c01b8b2e63a0fcd7be79ec41255cc18c9bfdae
Hunter MacNab <hunter.amac@gmail.com>
2025-02-28 17:02:42 -0800

    annalyn's infiltration

---
+++ ExcercismCSharp/AnnalynsInfiltration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcercismCSharp
+{
+    public static class QuestLogic
+    {
+        public static bool CanFastAttack(bool knightIsAwake)
+        {
+            return !knightIsAwake;
+        }
+
+        public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+        {
+            return knightIsAwake | archerIsAwake | prisonerIsAwake;
+        }
+
+        public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+        {
+            return !archerIsAwake && prisonerIsAwake;
+        }
+
+        public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+        {
+            return (petDogIsPresent && !archerIsAwake) | (prisonerIsAwake && !knightIsAwake && !archerIsAwake);
+        }
+    }
+}
---
+++ ExcersicmCSharp_MSTest/Test_AnnalynsInfiltration.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExcercismCSharp;
+
+namespace ExcersicmCSharp_MSTest
+{
+    namespace TestAnnalynsInfiltration { 
+
+        [TestClass]
+        public class FastAttack
+        {
+            [TestMethod]
+            public void CanCall()
+            {
+                QuestLogic.CanFastAttack(true);
+            }
+
+            [TestMethod]
+            public void KnightAwake()
+            {
+                Assert.AreEqual(false, QuestLogic.CanFastAttack(true));
+            }
+
+            [TestMethod]
+            public void KnightSleeping()
+            {
+                Assert.AreEqual(true, QuestLogic.CanFastAttack(false));
+            }
+        }
+
+        [TestClass]
+        public class CanSpy
+        {
+            [TestMethod]
+            public void CanCall()
+            {
+                QuestLogic.CanSpy(false, false, false);
+            }
+
+            [TestMethod]
+            public void NoOneAwake()
+            {
+                Assert.AreEqual(false, QuestLogic.CanSpy(false, false, false));
+            }
+
+            [TestMethod]
+            public void KnightAwake()
+            {
+                Assert.AreEqual(true, QuestLogic.CanSpy(true, false, false));
+            }
+        }
+
+        [TestClass]
+        public class CanFreePrisoner
+        {
+            [TestMethod]
+            public void HaveDog_ArcherAsleep()
+            {
+                Assert.AreEqual(true, QuestLogic.CanFreePrisoner(true, false, false, true));
+            }
+
+            [TestMethod]
+            public void HaveDog_ArcherAwake()
+            {
+                Assert.AreEqual(false, QuestLogic.CanFreePrisoner(true, true, false, true));
+            }
+
+            [TestMethod]
+            public void Sneaky_KnightArcherAsleep()
+            {
+                Assert.AreEqual(true, QuestLogic.CanFreePrisoner(false, false, true, false));
+            }
+
+            [TestMethod]
+            public void Sneaky_OneAwake()
+            {
+                Assert.AreEqual(false, QuestLogic.CanFreePrisoner(true, true, false, true));
+            }
+        }
+    }
+}
