3595794447f303b2dec22a2331d717139f03830f
Hunter MacNab <hunter.amac@gmail.com>
2025-03-06 15:43:40 -0800

    CarsAssemble_t1

---
+++ ExcercismCSharp/CarsAssemble.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcercismCSharp
+{
+    public static class AssemblyLine
+    {
+        //public static double Succe
+        public static double SuccessRate(int speed)
+        {
+            if(speed == 0) return 0;
+
+            if (speed >= 1 && speed <= 4) return 1;
+
+            if (speed >= 5 && speed <= 8) return 0.9;
+
+            if (speed == 9) return 0.8;
+
+            if (speed >= 10) return 0.77;
+
+            return 0;
+        }
+    }
+}
---
+++ ExcersicmCSharp_MSTest/Test_CarsAssemble.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExcercismCSharp;
+
+namespace ExcersicmCSharp_MSTest
+{
+    namespace TestCarsAssemble
+    {
+        [TestClass]
+        public class SuccessRate
+        {
+            [TestMethod]
+            public void CanCall()
+            {
+                AssemblyLine.SuccessRate(10);
+            }
+
+            [TestMethod]
+            public void AssemblerSpeedZero()
+            {
+                Assert.AreEqual(0, AssemblyLine.SuccessRate(0));
+            }
+
+            [TestMethod]
+            public void AssemblerSpeedTeir1()
+            {
+                Assert.AreEqual(1, AssemblyLine.SuccessRate(1));
+                Assert.AreEqual(1, AssemblyLine.SuccessRate(4));
+            }
+
+            [TestMethod]
+            public void AssemblerSpeedTeir2()
+            {
+                Assert.AreEqual(0.9, AssemblyLine.SuccessRate(5));
+                Assert.AreEqual(0.9, AssemblyLine.SuccessRate(8));
+            }
+
+            [TestMethod]
+            public void AssemblerSpeedTeir3()
+            {
+                Assert.AreEqual(0.8, AssemblyLine.SuccessRate(9));
+            }
+
+            [TestMethod]
+            public void AssemblerSpeedTeir4()
+            {
+                Assert.AreEqual(0.77, AssemblyLine.SuccessRate(10));
+            }
+
+            [TestMethod]
+            public void SpeedOutOfBounds()
+            {
+                // I think if speed is greater then max, it should default to max
+                Assert.AreEqual(0.77, AssemblyLine.SuccessRate(11));
+
+                // lower bound, default to minimum
+                Assert.AreEqual(0, AssemblyLine.SuccessRate(-1));
+            }
+        }
+    }
+}
