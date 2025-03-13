7dba0dfb3dc36af3cd610ab00dd4e0612e891d1e
Hunter MacNab <hunter.amac@gmail.com>
2025-03-13 16:30:44 -0700

    DifferenceOfSquares: t1, naive attempt without libraries

---
+++ ExcercismCSharp/DifferenceOfSquares.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        int sum = 0;
+        for(int i = 0; i<=max; ++i)
+        {
+            sum += i;
+        }
+        return sum*sum;
+    }
+}
---
+++ ExcersicmCSharp_MSTest/Test_DifferenceOfSquares.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExcercismCSharp;
+
+namespace ExcersicmCSharp_MSTest
+{
+    namespace TestDifferenceOfSquares { 
+        [TestClass]
+        public class CalculateSquareOfSum
+        {
+            [TestMethod]
+            public void SquareOfZero()
+            {
+                Assert.AreEqual(DifferenceOfSquares.CalculateSquareOfSum(0),0);
+            }
+
+            [TestMethod]
+            public void SqaureOfOne()
+            {
+                Assert.AreEqual(DifferenceOfSquares.CalculateSquareOfSum(1),1);
+            }
+
+            [TestMethod]
+            public void SqaureOfSumToTwo()
+            {
+                Assert.AreEqual(DifferenceOfSquares.CalculateSquareOfSum(2), 9);
+            }
+        }
+    }
+}
