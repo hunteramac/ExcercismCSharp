ba1256b74d1b17f6de9ef6d4049573928d58cf53
Hunter MacNab <hunter.amac@gmail.com>
2025-03-20 16:53:43 -0700

    init Game of Life

---
+++ ExcercismCSharp/GameOfLife.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcercismCSharp
+{
+    public static class GameOfLife
+    {
+        public static int[,] Tick(int[,] matrix)
+        {
+            return matrix;
+        }
+    }
+
+}
---
+++ ExcersicmCSharp_MSTest/Test_GameOfLife.cs
@@ -0,0 +1,40 @@
+﻿using ExcercismCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcersicmCSharp_MSTest
+{
+    namespace TestGameOfLife
+    {
+        [TestClass]
+        public class Tick
+        {
+            [TestMethod]
+            public void CanCall()
+            {
+                int[,] matrix = new int[,] {
+                    { 0, 0 },
+                    { 1, 0 },
+                };
+                GameOfLife.Tick(matrix);
+            }
+
+            [TestMethod]
+            public void OneCellDead()
+            {
+                int[,] inputMatrix = new int[,] {
+                    { 0 }
+                };
+
+                int[,] resultMatrix = new int[,] {
+                    { 0 }
+                };
+
+                Assert.AreEqual(GameOfLife.Tick(inputMatrix),resultMatrix);
+            }
+        }
+    }
+}
