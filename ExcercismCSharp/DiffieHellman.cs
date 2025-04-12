d8ef454d9a204ab298e2d56a2cd32a060af62a2c
Hunter MacNab <hunter.amac@gmail.com>
2025-04-11 17:06:11 -0700

    start DiffieHellman

---
+++ ExcercismCSharp/DiffieHellman.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ExcercismCSharp
+{
+   public static class DiffieHellman
+    {
+        public static BigInteger PrivateKey(BigInteger primeP)
+        {
+            if (primeP - 1 == 1)
+                throw new Exception("primeP must be larger then 2");
+            return primeP-1;
+            //throw new NotImplementedException("You need to implement this method.");
+        }
+
+        public static BigInteger PublicKey(BigInteger primeP, BigInteger primeG, BigInteger privateKey)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+
+        public static BigInteger Secret(BigInteger primeP, BigInteger publicKey, BigInteger privateKey)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
---
+++ ExcersicmCSharp_MSTest/Test_DiffieHellman.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExcercismCSharp;
+
+namespace ExcersicmCSharp_MSTest
+{
+    namespace TestDiffieHellman
+    {
+        [TestClass]
+        public class Dummy
+        {
+            [TestMethod]
+            public void Test()
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestClass]
+        public class PrivateKey
+        {
+            [TestMethod]
+            public void IsGreaterThenOne()
+            {
+                BigInteger primeP = 11;
+                BigInteger result = DiffieHellman.PrivateKey(primeP);
+                Assert.IsTrue(result > 1, "Expected PrivateKey(" + result + ") to be greater than 1");
+            }
+
+            [TestMethod]
+            public void IsLessThenParameter()
+            {
+                BigInteger primeP = 11;
+                BigInteger result = DiffieHellman.PrivateKey(primeP);
+                Assert.IsTrue(result < primeP, "Expected PrivateKey(" + result + ") to be less than PrimeP(" + primeP + ").");
+            }
+
+            [TestMethod]
+            public void PrimeNumberTwoIsExceptionCase()
+            {
+                BigInteger primeP = 2;
+                Assert.ThrowsException<Exception>(() => DiffieHellman.PrivateKey(primeP));
+            }
+        }
+    }
+}
